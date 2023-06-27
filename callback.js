c10b5359e8536776fcf48897b939f5af27df82d1
sairam-maurya <rams44258@gmail.com>
2023-06-27 01:53:37 -1200

    callback

--- callback.js
+++ callback.js
@@ -11,20 +11,51 @@
 
 
 let stocks = {
-    fruits:["strawberry","grapes","banana","apple"],
+    fruits: ["strawberry", "grapes", "banana", "apple"],
     liquid: ["water", "ice"],
     holder: ["cone", "cup", "stick"],
     toppings: ["chocolate", "peanuts"]
 };
 
 
-let order = (call_production)=>{
-    console.log("order placed")
-    call_production()
+let order = (fruit_name, call_production) => {
+
+    setTimeout(() => {
+        console.log(`${stocks.fruits[fruit_name]} was selected`)
+        call_production()
+
+    }, 2000)
+
 }
 
-let production =()=>{
-    console.log("production start")
+let production = () => {
+    setTimeout(() => {
+        console.log("production started")
+        setTimeout(() => {
+            console.log("the fruit had been chopped")
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+
+                setTimeout(() => {
+                    console.log("the machine had been started")
+                    setTimeout(() => {
+                        console.log(`${stocks.holder[0]} was selected`)
+                        setInterval(() => {
+                            console.log(`${stocks.toppings[0]} was added`)
+                            setTimeout(() => {
+                                console.log("serve icecream")
+                            }, 2000)
+                        }, 3000)
+                    },2000)
+                },1000)
+                
+                 }, 1000)
+
+        }, 2000)
+
+
+    })
+
 }
 
-order(production)
+order("0", production)
